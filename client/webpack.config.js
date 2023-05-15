3228e5a582eb8cc7ed6e22d8f6934c9a80b21f98
Leah <leahxharden@gmail.com>
2023-05-15 15:27:07 -0400

    typos fixed
    started work on Manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const { Cssloader } = require('css-loader');
+const { cssLoader } = require('css-loader');
 
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
@@ -17,6 +17,27 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunks: ['main'],
+    }),
+    new WebpackPwaManifest({
+      name: 'PWA-Text-Splash',
+      short_name: 'Splash',
+      description: 'PWA-Text-Splash',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+    new InjectManifest({
+      swSrc: './src/service-worker.js',
+    }),
+
   ],
   module: {
     rules: [
@@ -31,6 +52,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
           },
         },
       },
