e4c7757f54eb2f1ac47ea0782945fd1ef6927d9b
Leah <leahxharden@gmail.com>
2023-06-02 15:31:10 -0400

    WIP

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: './index.html',
       chunks: ['main'],
     }),
     new WebpackPwaManifest({
